a80619380100f0165a6948bd4390b2e7e59492f2
SergeyLp <serreg123@gmail.com>
2014-02-16 01:14:03 +0400

    Skip punctuation, numbers and detect end paragraph.

--- TxtUtil.cpp
+++ TxtUtil.cpp
@@ -1,8 +1,11 @@
 #include "stdafx.h"
-using namespace std;
+//using namespace std;
+using std::string;
+//using std::cout;
 
-const int  wordsInPrefix = 2;
-const char nonWord[] = "\n";	// cannot appear as real line: we remove newlines
+
+const int  wordsInPrefix = 1;
+const char nonWord[] = "{!}";	// cannot appear as real line
 const int  maxGeneratedWords = 1500;
 
 class PrefixWords{
@@ -19,45 +22,83 @@ public:
     Suffix(){};
     ~Suffix(){};
 
-    void push_back(const string &s){
+    inline void push_back(const string &s){
         wordList.push_back(s);
     }
 
-    const vector <string>& content()const{
+    inline const std::vector <string>& content()const{
         return wordList;
     }
 
 private:
-    vector<string> wordList;
+    std::vector<string> wordList;
 
 };
 
-typedef deque<string> Prefix;
-
-size_t prefixHash(const Prefix & prefix) {
-    size_t hash_ = 0;
-    for (const auto &item : prefix) {
-        hash_ ^= hash<string>()(item);
-    }
-    return hash_;
-}
+typedef std::deque<string> Prefix;
 
 //unordered_map<Prefix, vector<string>, decltype(&prefixHash) > statetab(1013, prefixHash); // prefix -> suffixes
-unordered_map<Prefix, Suffix, decltype(&prefixHash) > tabPrefixToSuffixes(1013, prefixHash);
+std::unordered_map<Prefix, Suffix, std::function<size_t(const Prefix & prefix) >>
+tablePrefixToSuffixes(1013, [](const Prefix & prefix){
+    size_t h = 0;
+    for (const string &x : prefix){
+        h ^= std::hash<string>()(x);
+    }
+    return h;
+});
 
-void		readAndBuild(Prefix&, istream&);
 void		generate();
-void		addWordToSuffixUpdatePrefix(Prefix&, const string&);
+void		add(Prefix&, const string&);
+
+bool isNumber(const std::string& s){
+    return (s.find_first_not_of("0123456789") == string::npos);
+    //return std::all_of(s.begin(), s.end(), ::isdigit); //Debug assert (c >= -1 && c <= 255) filed! 
+    //return !s.empty() && std::find_if(s.begin(), s.end(), [](unsigned char c) { return !std::isdigit(c); }) == s.end();
+}
 
 void initPrefix(Prefix &prefix){
     for (int i = 0; i < wordsInPrefix; i++)
-        addWordToSuffixUpdatePrefix(prefix, nonWord);
+        add(prefix, nonWord);
 }
 
+void readAndBuild(Prefix& prefix, std::ifstream &inputFile) {
+    string newLine, buf, buf1;
+    while (inputFile.good() || (inputFile.eof() && !newLine.empty())){
+        std::getline(inputFile, newLine, '\n');
+        if (newLine.empty())
+            continue;
+        std::stringstream ss(newLine);
+        while (ss >> buf){
+            //boost::trim_if(buf, boost::is_any_of("\"()?!.,«»-—“„")); //trimPunctuation
+            boost::trim_if(buf, !boost::algorithm::is_alpha());
+            if (buf.empty())
+                continue;
+
+            if (isNumber(buf)){
+                add(prefix, "{#}");
+                continue;
+            }
+            boost::algorithm::to_lower(buf);
+            add(prefix, buf);
+        }
+        add(prefix, "{/P}");
+    }
+}
 
 int main(int argc, char* argv[]) {
     std::locale::global(std::locale(""));
     setlocale(LC_ALL, "Russian");
+
+    std::string s = "Владимир Высоцкий";
+    std::cout << boost::algorithm::erase_first_copy(s, "и") << std::endl; 
+    std::cout << boost::algorithm::erase_nth_copy(s, "и", 2) << std::endl; 
+    std::cout << boost::algorithm::erase_last_copy(s, "и") << std::endl; 
+    std::cout << boost::algorithm::erase_all_copy(s, "и") << std::endl; 
+    std::cout << boost::algorithm::erase_head_copy(s, 5) << std::endl; 
+    std::cout << boost::algorithm::erase_tail_copy(s, 8) << std::endl;
+    //getchar();
+    //return 0;
+
 #if 0
     ::testing::InitGoogleTest(&argc, argv);
     const int res = RUN_ALL_TESTS( );
@@ -74,55 +115,57 @@ int main(int argc, char* argv[]) {
         std::cerr << "ERROR file not opened";
         return -1;
     }
+
     boost::timer::cpu_timer bench_timer;
     bench_timer.stop( );
 
     Prefix prefix;	// current input prefix
     initPrefix(prefix);
 
-    bench_timer.start( );
-    cout << "\nBuild ...";
+    std::cout << "\nBuild ...";
+    bench_timer.start();
     readAndBuild(prefix, input_file_stream);
     bench_timer.stop();
+    input_file_stream.close();
 
-    addWordToSuffixUpdatePrefix(prefix, nonWord);
+    add(prefix, nonWord);
     generate();
-    tabPrefixToSuffixes.clear();
+    tablePrefixToSuffixes.clear();
     //statetab.clear();
-    cout << "\n\b\b\b\t" << format(bench_timer.elapsed( )) << "";
+    std::cout << "\n\t" << format(bench_timer.elapsed()) << "";
     getchar();
 
     return 0;
 }
 
-void readAndBuild(Prefix& prefix, istream& in) {
-    string buf;
-
-    while (in >> buf)
-        addWordToSuffixUpdatePrefix(prefix, buf);
-}
-
 // add: add word to suffix deque, update prefix
-void addWordToSuffixUpdatePrefix(Prefix& prefix, const string& s) {
+void add(Prefix& prefix, const string& newWordSuffix) {
     if (prefix.size() == wordsInPrefix) {
-        //statetab[prefix].push_back(s);
-        tabPrefixToSuffixes[prefix].push_back(s);
+        //statetab[prefix].push_back(newWordSuffix);
+        tablePrefixToSuffixes[prefix].push_back(newWordSuffix);
         prefix.pop_front();
     }
-    prefix.push_back(s);
+    prefix.push_back(newWordSuffix);
 }
 
 void generate() {
-    for (const auto &it: tabPrefixToSuffixes){
-        const auto suffixesList = it.second.content();
-        if (suffixesList.size() < 2)
+    std::cout << '\n';
+    for (const auto &tablePair : tablePrefixToSuffixes){
+        const auto suffixesList = tablePair.second.content();
+        if (suffixesList.size() < 1)
             continue;
 
-        const auto &prefixWords = it.first;
-        cout << '\n' << prefixWords.at(0) << " " << prefixWords.at(1) << "\n";
-        for (const auto &suffixWord : suffixesList){
-            cout << "\t" << suffixWord << "\n";
+        const auto &prefixWords = tablePair.first;
+        for (const auto &prefixWord : prefixWords){
+            //if (prefixWord == )
+            std::cout << prefixWord << ' ';
         }
+        std::cout << "\n ";
+
+        for (const auto &suffixWord : suffixesList){
+            std::cout << "   " << suffixWord/* << "\n"*/;
+        }
+        std::cout << "\n";
     }
 
 #if 0
